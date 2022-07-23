75f44080654b8deb85732efbb986040aaaf194f9
Sahbaan Alam <sahbaanalam34@gmail.com>
2022-07-24 00:27:59 +0500

    version 0.1

--- src/main/java/spring/framework/stackholder/StackHolderApplication.java
+++ src/main/java/spring/framework/stackholder/StackHolderApplication.java
@@ -3,12 +3,15 @@ package spring.framework.stackholder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "spring.framework.stackholder")
 public class StackHolderApplication {
 
     public static void main(String[] args) {
