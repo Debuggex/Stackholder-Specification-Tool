efc612a7ba56da66214f7350027a344b1b3457eb
Sahbaan Alam <sahbaanalam34@gmail.com>
2022-07-24 02:04:54 +0500

    version 0.1

---
+++ src/main/java/spring/framework/stackholder/filter/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package spring.framework.stackholder.filter;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
