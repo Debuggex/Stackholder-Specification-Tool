644cf39f94ad77cc0c058cb0d76de73c589a5ed7
Sahbaan Alam <sahbaanalam34@gmail.com>
2022-07-24 01:01:20 +0500

    version 0.1

--- src/main/java/spring/framework/stackholder/filter/SecurityConfig.java
+++ src/main/java/spring/framework/stackholder/filter/SecurityConfig.java
@@ -1,11 +1,13 @@
 package spring.framework.stackholder.filter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +25,7 @@ import spring.framework.stackholder.Repositories.UserRepository;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     private final UserDetailsService userDetailsService;
 
     private final UserRepository userRepository;
@@ -74,7 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.addFilter(customAuthenticationFilter);
 
     }
-    @Bean
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
